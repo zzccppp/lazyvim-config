3bbb89359baaf715127da6871dd6029baf5bad47
ZZDirty <muztaga2@gmail.com>
2025-09-25 22:53:42 +0800

    add colorscheme / osc52 options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- unnamedplus 使用系統剪貼簿
+vim.opt.clipboard:append("unnamedplus")
+
+-- 使用 OSC 52
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
